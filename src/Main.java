103b4ec38b9fb4da3438ba79c3984913ebec151a
artem <agasyan222@gmail.com>
2024-10-09 23:46:05 +0400

    first

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+class Point {
+    double x, y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Point[] points = new Point[3];
+        for (int i = 0; i < 3; i++) {
+            points[i] = new Point(
+                    (i + 3) * 1.5,
+                    (i + 3) * 1.7
+            );
+        }
+
+        for (var point: points) {
+            System.out.println(point);
+        }
+    }
+}
